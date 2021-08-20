c37bed152583b84279ee7576e59fea02f7f08b43
Kevin McPeak <kevmc46@gmail.com>
2021-08-20 10:46:35 -0400

    added comments

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 console.log("Hello World");
 
-
-function computerPlay(){
+// function which generates random input from computer
+function computerPlay(){  
     let choice = Math.random()*3;
     let choicetwo = Math.floor(choice);
 
@@ -15,25 +15,30 @@ function computerPlay(){
     }
 };
 
+// asks user their selection
 function playerPlay(){
     selection = prompt("Please choose Rock, Paper, or Scissors")
     selection = selection.toLowerCase()
     return selection
 };
 
+// main function. 
 function playRound(playerSelection,computerSelection){
+
 
     console.log(`Player 1 choses ${playerSelection} while computer chooses ${computerSelection}`);
 
     if(playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
         document.getElementById("result").innerHTML = "Player 1 wins!";
+  
     } else if (playerSelection === computerSelection){
         document.getElementById("result").innerHTML="It is a tie!";
     } else{
         document.getElementById("result").innerHTML="You LOST. Computer wins";
     };
-    
 };
+
+
 
 
 
