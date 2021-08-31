3b6fb67fb25f53487458f0445faf61ce3028ab7d
Kevin McPeak <kevmc46@gmail.com>
2021-08-31 15:19:00 -0400

    Scoring and logic added

--- script.js
+++ script.js
@@ -25,30 +25,52 @@ function playerPlay(){
 const rock = document.querySelector("#rockbtn");
 const paper = document.querySelector('#paperbtn');
 const scissors = document.querySelector('#scissorsbtn');
+let playerScore = document.querySelector('#playertally');
+let computerScore = document.querySelector('#computertally');
+
+let p = 0;
+let c = 0;
 
 
-
-// main function. 
 function playRound(playerSelection,computerSelection){
 
     console.log(`Player 1 choses ${playerSelection} while computer chooses ${computerSelection}`);
 
+    while (p < 5 || c < 5){
+
     if(playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
         document.getElementById("result").innerHTML = "Player 1 wins!";
+        p+=1;
+        console.log(`Player 1 has a score of ${p}`);
+        playerScore.textContent = `${p}`;
+        if (p === 5){
+            playerScore.textContent = `That is ${p} wins.  PLAYER 1 is VICTORIOUS!`
+        }
+        break;
   
     } else if (playerSelection === computerSelection){
         document.getElementById("result").innerHTML="It is a tie!";
+        console.log(p);
+        break;
+
     } else{
         document.getElementById("result").innerHTML="You LOST. Computer wins";
+        c+=1;
+        console.log(`Computer has a score of ${c}`);
+        computerScore.textContent = `${c}`;
+        if (c === 5){
+        computerScore.textContent = `That is ${c} wins.  PLAYER 1 is VICTORIOUS!`};
+        break;
     };
+}
 };
 
-rock.addEventListener('click', () =>{
-    playRound('rock', computerPlay())});
-
-paper.addEventListener('click', () =>{
-    playRound('paper', computerPlay())});
-
-scissors.addEventListener('click', () =>{
-    playRound('scissors', computerPlay())});
     
+    rock.addEventListener('click', () =>{
+        playRound('rock', computerPlay())});
+    
+    paper.addEventListener('click', () =>{
+        playRound('paper', computerPlay())});
+    
+    scissors.addEventListener('click', () =>{
+        playRound('scissors', computerPlay())});
